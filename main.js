55ea547b66e7bc516e8cde6e18f8496f0bc4d3f3
arunkbelwal <arunblwl@gmail.com>
2017-08-10 14:04:04 +0530

    review comments fixed

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CurrencyConverterApp from './CurrencyConverterApp.jsx';
 
-ReactDOM.render(<CurrencyConverterApp />, document.getElementById('app'));
-ReactDOM.render(<CurrencyConverterApp />, document.getElementById('appone'));
-ReactDOM.render(<CurrencyConverterApp />, document.getElementById('appthree'));
+ReactDOM.render(<CurrencyConverterApp />,document.getElementById('app'));
+ReactDOM.render(<CurrencyConverterApp />,document.getElementById('appOne'));
+ReactDOM.render(<CurrencyConverterApp />,document.getElementById('appTwo'));
+
 
  
 
